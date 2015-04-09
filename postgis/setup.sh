047344540cd5900c32ad4ae0efe7b818e6460acb
Jonathan Meyer <jon@gisjedi.com>
2015-04-09 00:17:21 -0400

    Initial Dockerfiles and composer yml to spin up postgis and geoserver

---
+++ postgis/setup.sh
@@ -0,0 +1,10 @@
+# Add any additional setup tasks here
+
+# These tasks are run as root
+CONF="/etc/postgresql/9.3/main/postgresql.conf"
+
+# Restrict subnet to docker private network
+echo "host    all             all             172.17.0.0/16               md5" >> /etc/postgresql/9.3/main/pg_hba.conf
+# Listen on all ip addresses
+echo "listen_addresses = '*'" >> /etc/postgresql/9.3/main/postgresql.conf
+echo "port = 5432" >> /etc/postgresql/9.3/main/postgresql.conf
