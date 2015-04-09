0bb02c35cf635007d84f2cd3fff3f3c9b311a528
Jonathan Meyer <jon@gisjedi.com>
2015-04-09 01:34:41 -0400

    Staging MODIS data in PostGIS container

---
+++ geoserver/initialize-test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+$CATALINA_HOME/bin/startup.sh && sleep 15
+
+curl -v -u admin:geoserver -XPOST -H "Content-type: text/xml" -d "<workspace><name>test</name></workspace>" http://localhost:8080/geoserver/rest/workspaces
+
+curl -v -u admin:geoserver -XPOST -T test-store.xml -H "Content-type: text/xml" http://localhost:8080/geoserver/rest/workspaces/test/datastores
+
+$CATALINA_HOME/bin/shutdown.sh && sleep 15
