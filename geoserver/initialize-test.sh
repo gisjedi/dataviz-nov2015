aff9647bd24871e8dd1c8175491009eaee609392
Jonathan Meyer <jon@gisjedi.com>
2015-04-09 02:17:54 -0400

    WIP changes to use mdillon postgis instead of the supervisord from zakora

--- geoserver/initialize-test.sh
+++ geoserver/initialize-test.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-
-$CATALINA_HOME/bin/startup.sh && sleep 15
+$CATALINA_HOME/bin/startup.sh && sleep 5
 
 curl -v -u admin:geoserver -XPOST -H "Content-type: text/xml" -d "<workspace><name>test</name></workspace>" http://localhost:8080/geoserver/rest/workspaces
 
 curl -v -u admin:geoserver -XPOST -T test-store.xml -H "Content-type: text/xml" http://localhost:8080/geoserver/rest/workspaces/test/datastores
 
-$CATALINA_HOME/bin/shutdown.sh && sleep 15
+$CATALINA_HOME/bin/shutdown.sh && sleep 5
--- postgis/initial-data.sh
+++ postgis/initial-data.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Identify year for pulling data
 YEAR=$(date +'%Y')
 SOURCEPAGE=modisfire_${YEAR}_conus.htm
 
@@ -9,15 +10,30 @@ SHAPEFILEZIP=`cat $SOURCEPAGE |grep shapefile -m 1| cut -d "\"" -f 2`
 rm $SOURCEPAGE
 echo $SHAPEFILEZIP
 
+# Grab file name only and unzip dataset
 wget $SHAPEFILEZIP
 SHAPEFILEZIP=$(basename $SHAPEFILEZIP)
 unzip $SHAPEFILEZIP
 rm $SHAPEFILEZIP
 
+# Strip extension off name and convert shapefile to a SQL script
 SHAPEFILEPRE=${SHAPEFILEZIP/_shapefile/}
 SHAPEFILEPRE=`echo $SHAPEFILEPRE | cut -d "." -f 1`
 echo $SHAPEFILEPRE
-su - postgres -c "shp2pgsql -I -D -s 4326 $SHAPEFILEPRE.shp modis_${YEAR} | psql -U docker gis"
+shp2pgsql -I -D -s 4326 $SHAPEFILEPRE.shp modis_${YEAR} > $SHAPEFILEPRE.sql
 
+# Add test database
+POSTGRES="gosu postgres postgres"
+
+$POSTGRES --single -E <<EOSQL
+CREATE DATABASE testdb
+EOSQL
+
+# Add postgis extension and load data
+POSTGIS_CONFIG=/usr/share/postgresql/$PG_MAJOR/contrib/postgis-$POSTGIS_MAJOR
+$POSTGRES --single testdb -j < $POSTGIS_CONFIG/postgis.sql
+$POSTGRES --single testdb -j < $SHAPEFILEPRE.sql
+
+# Cleanup scratch files
 rm $SHAPEFILEPRE.*
 
