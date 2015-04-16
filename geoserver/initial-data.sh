201d3355f3a1c994c03108ae510bcb8b88f9573e
Jonathan Meyer <jon@gisjedi.com>
2015-04-16 15:18:04 -0400

    Added daycount layer

--- geoserver/initial-data.sh
+++ geoserver/initial-data.sh
@@ -5,3 +5,4 @@ curl -v -u admin:geoserver -XDELETE http://localhost:8080/geoserver/rest/workspa
 curl -v -u admin:geoserver -XPOST -H "Content-type: text/xml" -d "<workspace><name>test</name></workspace>" http://localhost:8080/geoserver/rest/workspaces
 curl -v -u admin:geoserver -XPOST -T ${DIR}/test-store.xml -H "Content-type: text/xml" http://localhost:8080/geoserver/rest/workspaces/test/datastores
 curl -v -u admin:geoserver -XPOST -T ${DIR}/modis-2015.xml -H "Content-type: text/xml" http://localhost:8080/geoserver/rest/workspaces/test/datastores/test/featuretypes
+curl -v -u admin:geoserver -XPOST -T ${DIR}/modis-2015_daycount.xml -H "Content-type: text/xml" http://localhost:8080/geoserver/rest/workspaces/test/datastores/test/featuretypes
