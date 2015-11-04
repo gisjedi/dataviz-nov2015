861a7603e61231e26a97ec4330f6e04232cfc886
Jonathan Meyer <jon@gisjedi.com>
2015-11-03 19:00:44 -0500

    Fixed VIIRS mosaic transparency. Corrected missed MODIS refactor workspace. Updated timeregex to be correct Java format codes for VIIRS.

--- geoserver/initial-modis.sh
+++ geoserver/initial-modis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-curl -v -u admin:geoserver -XDELETE http://localhost:8080/geoserver/rest/workspaces/test?recurse=true
+curl -v -u admin:geoserver -XDELETE http://localhost:8080/geoserver/rest/workspaces/feature?recurse=true
 curl -v -u admin:geoserver -XPOST -H "Content-type: text/xml" -d "<workspace><name>feature</name></workspace>" http://localhost:8080/geoserver/rest/workspaces
 curl -v -u admin:geoserver -XPOST -T ${DIR}/modis-store.xml -H "Content-type: text/xml" http://localhost:8080/geoserver/rest/workspaces/feature/datastores
 curl -v -u admin:geoserver -XPOST -T ${DIR}/modis-2015.xml -H "Content-type: text/xml" http://localhost:8080/geoserver/rest/workspaces/feature/datastores/modis/featuretypes
