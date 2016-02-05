36031f42dbcbf55f42addc25a7a4ad5686226feb
ericsvendsen <esvendsen@gmail.com>
2016-02-05 11:34:49 -0500

    correcting viirs-dnb to landsat

--- initial-landsat.sh
+++ initial-landsat.sh
@@ -4,7 +4,7 @@ POST_CONTAINER=`docker-compose ps -q postgis`
 GEOSERVER_CONTAINER=`docker-compose ps -q geoserver`
 docker exec ${GEOSERVER_CONTAINER} apt-get update -y
 docker exec ${GEOSERVER_CONTAINER} apt-get install curl wget -y
-docker exec ${GEOSERVER_CONTAINER} bash /tmp/geoserver/cleanup-viirs-dnb.sh
-docker exec ${POST_CONTAINER} su - postgres -c "bash /tmp/postgis/initial-viirs-dnb.sh"
+docker exec ${GEOSERVER_CONTAINER} bash /tmp/geoserver/cleanup-landsat.sh
+docker exec ${POST_CONTAINER} su - postgres -c "bash /tmp/postgis/initial-landsat.sh"
 
-docker exec ${GEOSERVER_CONTAINER} bash /tmp/geoserver/initial-viirs-dnb.sh
+docker exec ${GEOSERVER_CONTAINER} bash /tmp/geoserver/initial-landsat.sh
