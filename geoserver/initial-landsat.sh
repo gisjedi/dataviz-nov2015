f463e850188942b07b207c6ce809bdff0d64f960
ericsvendsen <esvendsen@gmail.com>
2016-02-08 14:12:00 -0500

    modifying wget

--- geoserver/initial-landsat.sh
+++ geoserver/initial-landsat.sh
@@ -22,23 +22,18 @@ cd ${DATA_DIR}
 
 #wget -nc -nd -np --follow-ftp -A "${ACCEPT_PATTERN}" -I "${DIR_ACCEPT_PATTERN}" -r "${FTP_URL}"
 
-S3_URL_LIST=['http://ais-landsat.s3.amazonaws.com/LC80270392015025LGN00.tar.gz',
-'http://ais-landsat.s3.amazonaws.com/LC80270392015073LGN00.tar.gz',
-'https://s3.amazonaws.com/ais-landsat/LC80270392015105LGN00.tar.gz',
-'https://s3.amazonaws.com/ais-landsat/LC80270392015121LGN00.tar.gz',
-'https://s3.amazonaws.com/ais-landsat/LC80270392015153LGN00.tar.gz',
-'https://s3.amazonaws.com/ais-landsat/LC80270392015201LGN00.tar.gz',
-'https://s3.amazonaws.com/ais-landsat/LC80270392015217LGN00.tar.gz',
-'https://s3.amazonaws.com/ais-landsat/LC80270392015249LGN00.tar.gz',
-'https://s3.amazonaws.com/ais-landsat/LC80270392015281LGN00.tar.gz',
-'https://s3.amazonaws.com/ais-landsat/LC80270392015313LGN00.tar.gz',
-'https://s3.amazonaws.com/ais-landsat/LC80270392015345LGN00.tar.gz',
-'https://s3.amazonaws.com/ais-landsat/LO80270392015041LGN00.tar.gz']
-
-# Expand archives
-for ARCHIVE in ${S3_URL_LIST}; do
-    wget -A "${ARCHIVE}" "${S3_URL}"
-done
+wget 'http://ais-landsat.s3.amazonaws.com/LC80270392015025LGN00.tar.gz'
+wget 'http://ais-landsat.s3.amazonaws.com/LC80270392015073LGN00.tar.gz'
+wget 'https://s3.amazonaws.com/ais-landsat/LC80270392015105LGN00.tar.gz'
+wget 'https://s3.amazonaws.com/ais-landsat/LC80270392015121LGN00.tar.gz'
+wget 'https://s3.amazonaws.com/ais-landsat/LC80270392015153LGN00.tar.gz'
+wget 'https://s3.amazonaws.com/ais-landsat/LC80270392015201LGN00.tar.gz'
+wget 'https://s3.amazonaws.com/ais-landsat/LC80270392015217LGN00.tar.gz'
+wget 'https://s3.amazonaws.com/ais-landsat/LC80270392015249LGN00.tar.gz'
+wget 'https://s3.amazonaws.com/ais-landsat/LC80270392015281LGN00.tar.gz'
+wget 'https://s3.amazonaws.com/ais-landsat/LC80270392015313LGN00.tar.gz'
+wget 'https://s3.amazonaws.com/ais-landsat/LC80270392015345LGN00.tar.gz'
+wget 'https://s3.amazonaws.com/ais-landsat/LO80270392015041LGN00.tar.gz'
 
 for ARCHIVE_FILE in `ls *.tar.gz`; do
     tar -xzvf ${ARCHIVE_FILE}
